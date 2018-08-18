ee7446cb2fee9d24e488ddca328511449b07e09f
Joey Cho <cjy12182000@naver.com>
2018-08-18 13:38:02 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -8,7 +8,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.string :costume_inventory
       t.string :num_of_employees
       t.boolean :still_in_business
-  #    t.string :opening_time
+      t.string :opening_time
   #    t.string :closing_time
     end
   end
