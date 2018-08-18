4ec4d128ebd4a6919017468d2e48692d26b8b519
Joey Cho <cjy12182000@naver.com>
2018-08-18 13:11:38 +0000

    automatically backed up by learn

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,8 +1,8 @@
 # Create your Costume class here
 # It should inherit from ActiveRecord::Base
 class Costume < ActiveRecord::Base
-  def created_at
-    puts "I know:)"
+#  def created_at
+#    puts "I know:)"
 
-  end
+#  end
 end
