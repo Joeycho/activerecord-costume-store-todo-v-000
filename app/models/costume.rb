5bcc5c346cd641d6673fdf7a48acf21112d69299
Joey Cho <cjy12182000@naver.com>
2018-08-18 13:15:08 +0000

    automatically backed up by learn

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -2,7 +2,5 @@ require 'pry'
 # Create your Costume class here
 # It should inherit from ActiveRecord::Base
 class Costume < ActiveRecord::Base
-  def created_at
-    self.created_at
-  end
+  
 end
