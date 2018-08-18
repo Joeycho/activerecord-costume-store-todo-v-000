6a1cff120de400d10126e7711374c4ba2a00d04b
Joey Cho <cjy12182000@naver.com>
2018-08-18 12:57:11 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -11,7 +11,7 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
 
   def change
     create_table :costumes do |t|
-      t.string :name
+      t.string :name,
       t.real :price
     end
   end
