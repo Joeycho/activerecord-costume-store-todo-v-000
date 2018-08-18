eb51b5ced2a13ec6bd5ef0935912a5d7945bf017
Joey Cho <cjy12182000@naver.com>
2018-08-18 13:48:15 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -11,7 +11,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
       t.datetime :opening_date
       t.datetime :closing_date
       t.text :description
-      binding.pry
+    #  binding.pry
     end
   end
 
